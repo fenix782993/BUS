f70282c584649ce42217b9d33ce2ebc7d153c05e
fenix782993 <kfeofilov52@gmail.com>
2026-09-22 01:48:05 +0300

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import "./styles.css";
 const root = document.getElementById("root");
 
 if (!root) {
-  throw new Error("FENIX CITY: элемент #root не найден");
+  throw new Error("FENIX CITY: #root not found");
 }
 
 createRoot(root).render(
